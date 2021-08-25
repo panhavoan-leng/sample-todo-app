870ff66eec91b5e4110e9ac8d07d0d613a10cdcb
l-panhavoan <l-sovanpanhavoan@beniten.com>
2021-08-25 18:17:21 +0700

    add delete and mark as complete endpoint

--- app/todo/urls.py
+++ app/todo/urls.py
@@ -4,14 +4,8 @@ from app.todo.views import TodoView
 urlpatterns = [
     path("", TodoView.as_view({"get": "list_todo"}), name="list_todo"),
     path("", TodoView.as_view({"post"}), name="create_todo"),
-    path(
-        "<int:id>/detail",
-        TodoView.as_view(
-            {
-                "get": "detail_todo",
-            }
-        ),
-        name="detail_todo",
-    ),
+    path("<int:id>/detail", TodoView.as_view({"get": "detail_todo"}), name="detail_todo"),
     path("<int:id>/update", TodoView.as_view({"put": "update_todo"}), name="update_todo"),
+    path("<int:id>/delete", TodoView.as_view({"delete": "delete_todo"}), name="delete_todo"),
+    path("<int:id>/mark_as_complete", TodoView.as_view({"put": "mark_as_complete"}), name="mark_as_complete"),
 ]
--- app/todo/views.py
+++ app/todo/views.py
@@ -4,14 +4,19 @@ from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import mixins
-from rest_framework import filters
 from rest_framework import viewsets
 from .serializers import TodoSerializer
 from .models import Todo
 
 
 class TodoView(
-    viewsets.ModelViewSet,
+    viewsets.ViewSet,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    generics.GenericAPIView,
 ):
 
     queryset = Todo.objects.all()
@@ -29,3 +34,13 @@ class TodoView(
 
     def update_todo(self, request, id=None):
         return self.update(request)
+
+    def delete_todo(self, request, id=None):
+        self.destroy(request)
+        return Response({"message": "OK"})
+
+    def mark_as_complete(self, request, id=None):
+        instance = self.get_object()
+        instance.is_completed = True
+        instance.save()
+        return Response({"message": "OK"})
